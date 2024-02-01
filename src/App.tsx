2b0a4d41f971f5fa2feb5b4bd7ca84843518dada
RamziNaili <naili.ramzi@outlook.fr>
2024-02-01 15:25:42 +0100

    feat: add default router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components';
 
 function App() {
   return (
-    <>
-      <p className="font-beViet">yeye</p>
-      <p>yeye</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
