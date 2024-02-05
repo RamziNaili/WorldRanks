a9c9023d12a0ef05ba048db3cc1275b0696cae97
RamziNaili <naili.ramzi@outlook.fr>
2024-02-05 11:43:50 +0100

    feat: get country and print theirs information

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,28 @@
 /// <reference types="vite-plugin-svgr/client" />
 
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Filter } from '../filter/Filter';
 import { TableContent } from '../TableContent/TableContent';
 import { SearchBar } from '../filter/SearchBar';
+import axios from 'axios';
 
 export const Home: FC = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get('https://restcountries.com/v3.1/all');
+        setData(res.data);
+        console.log('Data from API', data);
+      } catch (error) {
+        console.log('Error in Home component', error);
+      }
+    };
+    fetchData();
+    console.log('Home component mounted');
+  }, []);
+
   return (
     <div className="w-screen relative text-white">
       <img
@@ -13,7 +30,7 @@ export const Home: FC = () => {
         src="src/assets/hero-image.jpg"
         alt="hero"
       />
-      <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 p-4 border border-neutral-700 rounded-lg w-[90vw] ">
+      <div className="absolute top-[570px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 p-4 border border-neutral-700 rounded-lg w-[90vw] ">
         <div className="flex justify-between p-5">
           <p className="font-beViet font-semibold text-text ">
             Found 234 counties
@@ -22,7 +39,7 @@ export const Home: FC = () => {
         </div>
         <div className="grid grid-cols-[auto_1fr] mt-2">
           <Filter />
-          <TableContent />
+          <TableContent data={data} />
         </div>
       </div>
     </div>
--- src/components/TableContent/TableContent.tsx
+++ src/components/TableContent/TableContent.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react';
 
-export const TableContent: FC = () => {
+type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any;
+};
+
+export const TableContent: FC<Props> = ({ data }) => {
   return (
     <div>
-      <div className="grid grid-cols-[10%_30%_30%_15%_15%] text-text p-5">
+      <div className="grid grid-cols-[10%_35%_25%_15%_15%] text-text p-5">
         <p>Flag</p>
         <p>Name</p>
         <p>Population</p>
@@ -11,6 +16,22 @@ export const TableContent: FC = () => {
         <p>Region</p>
       </div>
       <span className="h-[1px] block bg-text w-full" />
+      <div className="bg-gray-50 max-h-[450px] overflow-y-scroll scrollbar-hide">
+        <div>
+          {data.map((country) => (
+            <div
+              key={`country-` + country.name.common}
+              className="grid grid-cols-[10%_35%_25%_15%_15%] grid-rows text-light p-5"
+            >
+              <p>a</p>
+              <p>{country.name.common}</p>
+              <p>{country.population}</p>
+              <p>{country.area}</p>
+              <p>{country.region}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
