a5f20469fe5df09cc92f5e634882622db2e3b45a
RamziNaili <naili.ramzi@outlook.fr>
2024-08-12 10:23:11 +0200

    change img import

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,11 @@
 import { FC } from 'react';
 import { HomePage } from '../../Pages/HomePage/HomePage';
+import img from 'src/assets/hero-image.jpg';
 
 export const Home: FC = () => {
   return (
     <div className="w-screen text-white">
-      <img
-        className="w-full h-[200px]"
-        src="src/assets/hero-image.jpg"
-        alt="hero"
-      />
+      <img className="w-full h-[200px]" src={img} alt="hero" />
       <div className="absolute shadow-xl top-[530px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 p-4 border border-neutral-700 rounded-lg w-[90vw] bg-primary">
         <HomePage />
       </div>
