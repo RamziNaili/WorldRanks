d3cba6cfb11e5ed019bbde1c2667a08d9d9f5d9b
RamziNaili <naili.ramzi@outlook.fr>
2024-11-13 12:39:56 +0100

    img wrong path

--- src/Pages/CountryPage/CountryPage.tsx
+++ src/Pages/CountryPage/CountryPage.tsx
@@ -37,11 +37,7 @@ export const CountryPage = () => {
 
   return (
     <div className="w-screen relative text-white">
-      <img
-        className="w-full h-[200px]"
-        src="src/assets/hero-image.jpg"
-        alt="hero"
-      />
+      <img className="w-full h-[200px]" src="/hero-image.jpg" alt="hero" />
       <div className="absolute shadow-xl top-[570px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-200 border border-neutral-700 rounded-lg w-[50vw] max-xl:w-[640px] max-md:w-full">
         {data && (
           <div className="h-[70vh] relative rounded-lg">
